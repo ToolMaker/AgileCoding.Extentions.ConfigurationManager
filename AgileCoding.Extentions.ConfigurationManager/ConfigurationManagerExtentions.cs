c9cce2ca3e4d3794b534e411d96ae5c73da02461
Ernie <gunning.ernie@gmail.com>
2021-06-01 19:29:28 +0200

    Project Start

---
+++ AgileCoding.Extentions.ConfigurationManager/ConfigurationManagerExtentions.cs
@@ -0,0 +1,36 @@
+﻿namespace AgileCoding.Extentions.ConfigurationManagers
+{
+    using System.Configuration;
+    using System.IO;
+    using AgileCoding.Extentions.Files;
+
+    public static class ConfigurationManagerExtentions
+    {
+        public static Configuration GetDLLConfiguration(string DLLNameAndExtention)
+        {
+            string detectedApp = "Application";
+            if (!DLLNameAndExtention.ToLower().EndsWith(".dll"))
+            {
+                throw new ConfigurationErrorsException($"DLL Name '{DLLNameAndExtention}' doesnt end with the '.dll' extention. I need the FULL dll name in order to load the dll config.");
+            }
+
+            var rootConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            FileInfo rootConfigFile = new FileInfo(rootConfig.FilePath);
+            var rigthConfigFile = string.Empty;
+            if (rootConfigFile.Name.ToLower().EndsWith($".exe.config"))
+            {
+                //Its a application running, so the DLL configfiles should sit in the root
+                rigthConfigFile = $@"{rootConfigFile.Directory.FullName}\{DLLNameAndExtention}";
+            }
+            else if (rootConfigFile.Name.ToLower().EndsWith($"web.config"))
+            {
+                detectedApp = "Web Applicaiton";
+                //Its a WEB application running, so the DLL configfiles should sit in the bin
+                rigthConfigFile = $@"{rootConfigFile.Directory.FullName}\bin\{DLLNameAndExtention}";
+            }
+            rigthConfigFile.ThrowIfFileDoesntExist<ConfigurationErrorsException>($"Unable to find config file for {detectedApp} at '{rigthConfigFile}'");
+
+            return ConfigurationManager.OpenExeConfiguration(rigthConfigFile);
+        }
+    }
+}
---
+++ ReadMePage.md
@@ -0,0 +1,5 @@
+﻿#AgileCoding.Extentions.ConfigurationManager
+
+###Extensions related to ConfiguraitonManager functionality
+
+1.	ConfigurationManagerExtentions - Static class containing ConfigurationManager Static methods
